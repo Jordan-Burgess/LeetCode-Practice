86af8a10d48c84ee71bc8a86099302b8b71b052c
Jordan Burgess <107363999+Jordan-Burgess@users.noreply.github.com>
2022-11-26 16:01:05 -0500

    Time: 22 ms (85.68%), Space: 13.4 MB (71.01%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        nums = [str(digit) for digit in digits]
-        value = str(int(''.join(nums)) + 1)
-        return [int(num) for num in value]
+ 
+        # nums = [str(digit) for digit in digits]
+        # value = str(int(''.join(nums)) + 1)
+        # return [int(num) for num in value]
+    
+        return [int(num) for num in str(int(''.join([str(digit) for digit in digits])) + 1)]
