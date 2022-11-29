e4d7f95a532925d312fe1ec134e752624ad14256
Jordan Burgess <107363999+Jordan-Burgess@users.noreply.github.com>
2022-11-29 15:48:59 -0500

    Time: 284 ms (71.99%), Space: 14.7 MB (26.44%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        
+        while low <= high:
+            mid = high + low // 2
+            
+            if target > nums[mid]:
+                low = mid + 1
+            elif target < nums[mid]:
+                high = mid - 1
+            else:
+                return mid
+        return -1
+        
+        
